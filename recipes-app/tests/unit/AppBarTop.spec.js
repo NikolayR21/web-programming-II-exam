c05abfc7ddd78273e436a7023f5da342df100d5c
NikolayR21 <nikolay.rusev21@gmail.com>
2022-05-31 00:52:05 +0200

    added test to check if it contains an img

--- recipes-app/tests/unit/AppBarTop.spec.js
+++ recipes-app/tests/unit/AppBarTop.spec.js
@@ -1,13 +1,16 @@
 import { mount } from '@vue/test-utils'
 import AppBarTop from '@/components/AppBarTop.vue'
 
-
 describe('AppBarTop unit testing: ', () => {
-  test('is a Vue instance', () => {
+  it('is a Vue instance', () => {
     const wrapper = mount(AppBarTop);
 
     expect(wrapper.isVueInstance()).toBeTruthy();
   })
 
-  
+  it('contains an img', () => {
+    const wrapper = mount(AppBarTop);
+
+    expect(wrapper.contains('img')).toBeTruthy();
+  })
 })
