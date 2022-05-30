9612deec731f713642776113bdec5bca4b0dcc35
NikolayR21 <1081601@ucn.dk>
2022-05-30 19:14:21 +0200

    added git config for auth

--- recipes-app/deploy.sh
+++ recipes-app/deploy.sh
@@ -13,5 +13,5 @@ git init
 git add .
 git commit -m "deploying commit"
 
-git push https://${{ secrets.GITHUB_TOKEN }}@github.com/NikolayR21/web-programming-II-exam.git master:gh-pages
+git push -f https://github.com/NikolayR21/web-programming-II-exam.git master:gh-pages
 cd ..
