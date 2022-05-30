dd51a18ad252c902ec10310fb7c0ae556112e953
NikolayR21 <1081601@ucn.dk>
2022-05-30 17:15:20 +0200

    testing deploy

---
+++ recipes-app/deploy.sh
@@ -0,0 +1,18 @@
+#build /dist
+npm run build
+
+#get the latest commit hash as a string
+LOGSTRING=$(git log)
+COMMIT=$(echo $LOGSTRING | awk '{print $2}') 
+
+#navigato to dist
+cd dist
+
+#init a repo and commit
+git init
+git add .
+git commit -m "deploying commit"
+
+git push -f https://github.com/NikolayR21/web-programming-II-exam.git master:gh-pages
+
+cd ..
